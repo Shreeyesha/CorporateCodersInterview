8a575317e299b7493fcdd0b9bb3ea501d557f3e7
Shreeyesha <75382692+Shreeyesha@users.noreply.github.com>
2020-12-02 22:02:12 +0530

    Create TestQ2.php
    Everyday, the restaurant has a limited number of breads, vadas and samosas. The
    restaurant needs to optimise orders in a way that they can generate maximum profit.
    Vadapav needs two bread and a vada
    Samosapav needs two bread and a samosa
    Input:
    First input is: no. of breads, no. of vadas and no. of samosas available in the
    restaurant
    Second input is: Price of Vadapav & price of Samosapav set by the restaurant
    Output:
    Maximum profit possible
    Example:
    Input:
    9 2 3 (9 breads, 2 vada, 3 samosa)
    10 15 (Rs.10 - Vadapav, Rs.15 - Samosapav)
    Output:
    55 (Maximum profit possible is Rs.55)

---
+++ TestQ2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TestQ2</title>
+</head>
+<body>
+<form action="TestQ2.php" method="POST">
+	Number of breads, vadas and samosas : <input type="text" name="ingre"><br>
+	Price of Vadapava and Samosapav : <input type="text" name="price"><br>
+	<button type="submit" name="submit">Submit</button>
+</form>
+
+<?php 
+if(isset($_POST["submit"]))
+	{
+		$ingredients=explode(" ", $_POST["ingre"]);
+		$price=explode(" ", $_POST["price"]);
+		$bread=intdiv($ingredients[0], 2);
+		$vadas=$ingredients[1];
+		$samosas=$ingredients[2];
+		$max_vadas=min($bread,$vadas);
+		$max_samosas=min($bread,$samosas);
+		$cost_vada=$price[0];
+		$cost_samosa=$price[1];
+		$max_profit=0;
+
+		for($i=1;$i<=$max_vadas;$i++)  //assuming from the example that atleast one of vadapav and samosapav each has to be sold.
+		{
+			if(($bread-$i<=$max_samosas)&&($bread-$i)>0)
+			{
+				$sales=$i*$cost_vada+($bread-$i)*$cost_samosa;
+				$max_profit=max($max_profit,$sales);
+			}
+		}
+		print "Maximum profit possible: ".($max_profit);
+	}
+?> 	
+</body>
+</html>
