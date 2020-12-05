08482f9c83257bb7099cbd32a7325aadb476c3f0
Shreeyesha <75382692+Shreeyesha@users.noreply.github.com>
2020-12-05 15:33:36 +0530

    Update TestQ1.php

--- TestQ1.php
+++ TestQ1.php
@@ -7,29 +7,45 @@
 
 	<form action="TestQ1.php" method="POST">
 		Number of servers : <input type="number" name="no_servers"><br>
-		Server load every minute : <input type="text" name="server_load"><br>
+		Server load every minute :<input type="text" name="server_load"><br>
 		<button type="submit" name="submit">Submit</button>
 	</form>
 
-	<?php
-
-	if(isset($_POST["submit"]))
+<?php
+if(isset($_POST["submit"]))
 	{
 		$num_of_servers=$_POST["no_servers"];
-		$server_load=explode(" ", $_POST["server_load"]);
+		$server_load=explode(" ", $_POST["server_load"]);    
+		
+	 
+		for($i=0;$i<5;$i++)
+		{
+			
+			if ($server_load[$i] < 50)
+			{
+				if(intdiv($num_of_servers,2)<1)
+				{
+					$num_of_servers=1;
+				}
+				else
+				{
+					$num_of_servers=intdiv($num_of_servers,2);
+				}
 
-		$sum = array_sum($server_load);
-		if ($sum/5 < 50)
-		{
-			print "Number of servers : ".$num_of_servers/2;
+			}
+			else
+			{
+				$num_of_servers=(($num_of_servers*2)+1);
+			}
+
+			
 		}
-		else
-		{
-			print "Number of servers : ".(($num_of_servers*2)+1);
-		}
+
+		print("<br>The number of servers working after 5 minutes is : ".$num_of_servers);
 
 	}
+
 	?>
 
-</body>
+	</body>
 </html>
